10fb9e523447f1ece444917c12b322f86149fb8d
Marcel Schoen <marcel.schoen@united-security-providers.ch>
2024-02-16 20:06:29 +0000

    Working on Githup pages (#273114)

--- release.sh
+++ release.sh
@@ -4,20 +4,25 @@ DIR=`pwd`
 rm -rf build
 rm -rf docs
 
-# Determine
+# Determine last release version
 mkdir build
 cd build
-echo "--> clone..."
+# Clone meta information of operator project
 git clone -n git@git.u-s-p.local:core-waap/core-waap-operator.git
-echo "--> done."
 cd core-waap-operator
+# Get last version GIT tag
 export RELEASE=`git tag --sort=creatordate -l *.*.* | tail -1`
 
 echo "Last operator release: $RELEASE"
 
+# Download changelog of operator release
 wget http://nexus-bob.u-s-p.local/repository/releases/ch/u-s-p/core/waap/waap-operator/$RELEASE/waap-operator-$RELEASE-changelog.md
 
 # Prepare site source directory
 cd $DIR
 cp -R src/docs ./docs
 cp build/core-waap-operator/waap-operator-0.2.0-changelog.md ./docs/CHANGELOG.md
+
+# Deploy to Github pages
+mkdocs gh-deploy
+
